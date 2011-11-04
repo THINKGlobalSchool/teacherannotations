ebfb422f4021826cf8504bb2647adffa9165c43a
Jeff Tilson <jrtilson@gmail.com>
2011-11-04 12:18:18 -0400

    Initial Commit

---
+++ start.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Teacher Annotations Start.php
+ *
+ * @package TeacherAnnotations
+ * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU Public License version 2
+ * @author Jeff Tilson
+ * @copyright THINK Global School 2010
+ * @link http://www.thinkglobalschool.com/
+ * 
+ */
+
+elgg_register_event_handler('init', 'system', 'teacher_annotations_init');
+
+function teacher_annotations_init() {
+
+}
