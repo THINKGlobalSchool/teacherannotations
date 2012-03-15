4a4f739616375c48924714ca658824dae9f334cb
Jeff Tilson <jrtilson@gmail.com>
2012-03-15 15:32:39 -0400

    Not sure why I had code to fix youtube iframes in this plugin.. removing it

--- views/default/js/teacherannotations/teacherannotations.php
+++ views/default/js/teacherannotations/teacherannotations.php
@@ -16,12 +16,6 @@ elgg.provide('elgg.teacherannotations');
 // Init function
 elgg.teacherannotations.init = function() {
 	$("div#ta-bottom-bar").appendTo('body').css({height: 0}).animate({ height: '18' }, 'slow');
-
-	// Fix broken youtube embed
-	$('iframe').each(function() {
-		var url = $(this).attr("src");
-		$(this).attr("src",url+"?wmode=transparent");
-	});
 }
 
 elgg.register_hook_handler('init', 'system', elgg.teacherannotations.init);
